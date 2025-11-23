720b740fe92d1ce03b205acc5c0f4b99a9599dfe
Yan Moroz <>
2025-11-23 22:51:51 +0300

    feat: flutter_svg

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,88 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/applications-icon.svg
+  String get applicationsIcon => 'assets/icons/applications-icon.svg';
+
+  /// File path: assets/icons/back-icon.svg
+  String get backIcon => 'assets/icons/back-icon.svg';
+
+  /// File path: assets/icons/bell-icon.svg
+  String get bellIcon => 'assets/icons/bell-icon.svg';
+
+  /// File path: assets/icons/comments-icon.svg
+  String get commentsIcon => 'assets/icons/comments-icon.svg';
+
+  /// File path: assets/icons/contacts-icon.svg
+  String get contactsIcon => 'assets/icons/contacts-icon.svg';
+
+  /// File path: assets/icons/cross-icon.svg
+  String get crossIcon => 'assets/icons/cross-icon.svg';
+
+  /// File path: assets/icons/discounts-icon.svg
+  String get discountsIcon => 'assets/icons/discounts-icon.svg';
+
+  /// File path: assets/icons/home-icon.svg
+  String get homeIcon => 'assets/icons/home-icon.svg';
+
+  /// File path: assets/icons/like-icon.svg
+  String get likeIcon => 'assets/icons/like-icon.svg';
+
+  /// File path: assets/icons/like-white-icon.svg
+  String get likeWhiteIcon => 'assets/icons/like-white-icon.svg';
+
+  /// File path: assets/icons/more-icon.svg
+  String get moreIcon => 'assets/icons/more-icon.svg';
+
+  /// File path: assets/icons/polls-icon.svg
+  String get pollsIcon => 'assets/icons/polls-icon.svg';
+
+  /// File path: assets/icons/resell-icon.svg
+  String get resellIcon => 'assets/icons/resell-icon.svg';
+
+  /// File path: assets/icons/s8-icon.svg
+  String get s8Icon => 'assets/icons/s8-icon.svg';
+
+  /// File path: assets/icons/telegram-icon.svg
+  String get telegramIcon => 'assets/icons/telegram-icon.svg';
+
+  /// List of all assets
+  List<String> get values => [
+    applicationsIcon,
+    backIcon,
+    bellIcon,
+    commentsIcon,
+    contactsIcon,
+    crossIcon,
+    discountsIcon,
+    homeIcon,
+    likeIcon,
+    likeWhiteIcon,
+    moreIcon,
+    pollsIcon,
+    resellIcon,
+    s8Icon,
+    telegramIcon,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const String aEnv = '.env';
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+
+  /// List of all assets
+  static List<String> get values => [aEnv];
+}
